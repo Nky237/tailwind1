4202c36ff13e3bacd832385e979976a40cf15aae
Ezike Nkemdili Jane <ezikenkemdilijane@gmail.com>
2022-06-10 15:29:33 +0100

    First commit

---
+++ src/component/Hello.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function Hello(props){
+   
+   
+    return(
+        <h1>Heloo world ghjll {props.name} </h1>
+    
+        
+        
+    )
+}
+
+export default Hello;
